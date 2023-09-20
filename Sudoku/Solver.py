607b802c5bef74a6f084dc5eb15960756d717c46
Gustavo Teixeira Magalhães <gutemagae@gmail.com>
2023-09-19 22:24:23 -0300

    reestruturação da função main, e bateria de testes

--- Sudoku/Solver.py
+++ Sudoku/Solver.py
@@ -2,27 +2,6 @@ from Board import Board
 
 
 class Solver:
-    # @staticmethod
-    # def safePosition(num, tabuleiro, linha, coluna):
-    #     if num not in range(1, 10):
-    #         return False
-    #     elif tabuleiro[linha][coluna] != 0:
-    #         return False
-    #     elif Board.verificaLinha(num, tabuleiro, linha) and Board.verificaColuna(num, tabuleiro, coluna) and Board.verificaQuadrante(num, tabuleiro, linha, coluna):
-    #         return True
-    #
-    # @staticmethod
-    # def solve(tabuleiro, iLinha=0):
-    #     if iLinha == len(tabuleiro): # if Board.isComplete(tabuleiro):
-    #         return True
-    #
-    #     for num in range(1,10):
-    #         for coluna in range(0, len(tabuleiro)):
-    #             if Solver.safePosition(num, tabuleiro, iLinha, coluna):
-    #                 tabuleiro[iLinha][coluna] = num
-    #                 Solver.solve(tabuleiro, iLinha+1)
-    #                 tabuleiro[iLinha][coluna] = 0
-
     @staticmethod
     def whichIsSafe(tabuleiro, linha, coluna):
         possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]

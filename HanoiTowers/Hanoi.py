278bdeca096655e1ae43174841771634b92ac876
Gustavo Teixeira Magalhães <gutemagae@gmail.com>
2023-09-27 20:56:20 -0300

    Correção de conflitos entre branches

--- HanoiTowers/Hanoi.py
+++ HanoiTowers/Hanoi.py
@@ -1,3 +1,23 @@
+import os
+import time
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def exibeTorres(A, B, C, t1, t2, t3):
+    cls()
+    tam = max(len(A), len(B), len(C)) 
+    for i in range(tam - 1, -1, -1):
+        for torre in [A, B, C]:
+            if len(torre) <= i:
+                print('\t\t', end='')
+            else:
+                print(f'\t   {torre[i]}  ', end='')
+        print()
+    
+    print('\t |', t1, '| \t |', t2, '| \t |', t3, '|')
+    time.sleep(1)
+
 class Hanoi:
     @staticmethod
     def hanoi(n, origem = 'A', destino = 'C', auxiliar = 'B'):
@@ -8,3 +28,15 @@ class Hanoi:
             print("Move de {} pra {}".format(origem, destino))
             Hanoi.hanoi(n-1, auxiliar, destino, origem)
 
+    @staticmethod
+    def hanoiPilha(n, origem, destino, auxiliar, t1 = 'A', t2 = 'C', t3 = 'B'):
+        if n == 1:
+            valor = origem.pop()
+            destino.append(valor)
+            exibeTorres(origem, auxiliar, destino, t1, t2, t3)
+        else:
+            Hanoi.hanoiPilha(n-1, origem, auxiliar, destino)
+            valor = origem.pop()
+            destino.append(valor)
+            exibeTorres(origem, auxiliar, destino, t1, t2, t3)
+            Hanoi.hanoiPilha(n-1, auxiliar, destino, origem)
--- HanoiTowers/main.py
+++ HanoiTowers/main.py
@@ -3,7 +3,6 @@ import time
 
 from Hanoi import Hanoi
 
-
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -13,7 +12,7 @@ def comoFunciona():
     print("Torres de hanoi é um jogo matemático com 3 regras simples:")
     print("Existem 3 torres, chamadas pinos, uma torre de origem ,uma de destino e uma auxiliar.")
     print(
-        "Na primeira torre existe um número de discos definido por quem está jogando. Os discos tem tamanho decrescente, ou seja, o disco do topo é o menor, e o do fundo, o maior")
+        "Na primeira torre existe um número de discos definido por quem está jogando. Os discos tem tamanho decrescente, ou seja, o disco do topo é o menor, e o do fundo, o maior.")
     print(
         "O objetivo do jogo é transferir os discos do pino de origem para o pino de destino, utilizando do auxiliar como apoio.")
     print("Quanto mais discos, mais trabalhoso é de resolver. Além disso, existem outras duas regras adicionais: ")
@@ -29,31 +28,31 @@ def comoFunciona():
         print(
             "O paradigma da indução é uma forma de resolver algorítmos muito comum, além de ser um modo fácil e rápido de resolver um problema.")
         print("A indução precisa de duas coisas básicas:")
-        print("1 - Um caso base")
-        print("2 - Um passo de indução, baseado no caso base, que resolva instâncias menores do problema")
+        print("1 - Um caso base.")
+        print("2 - Um passo de indução, baseado no caso base, que resolva instâncias menores do problema.")
         input("\n\nAperte qualquer tecla para continuar...")
         cls()
         print("\n\t\tResolvendo o problema das torres de Hanoi por meio da indução recursiva:\n")
         print(
             "Caso Base: Se existir somente um disco no pino de origem, mova-o para o pino de destino, e a torre estará completa!")
         print(
-            "Passo Indutivo: \n1 - Mova os n-1 pinos menores do pino de origem para o pino auxiliar, utilizando o pino de destino como auxiliar")
-        print("2 - Mova o pino maior do pino de origem para o pino de destino")
+            "Passo Indutivo: \n1 - Mova os n-1 pinos menores do pino de origem para o pino auxiliar, utilizando o pino de destino como auxiliar.")
+        print("2 - Mova o pino maior do pino de origem para o pino de destino.")
         print(
-            "2.1 - Caso não for possível realizar o passo anterior, mova os discos do pino auxiliar até o pino de destino utilizando o pino de origem como auxiliar")
-        print("3 - Repita o processo até chegar no caso base e resolver o problema")
+            "2.1 - Caso não for possível realizar o passo anterior, mova os discos do pino auxiliar até o pino de destino utilizando o pino de origem como auxiliar.")
+        print("3 - Repita o processo até chegar no caso base e resolver o problema.")
         input("\n\nAperte Qualquer tecla para continuar...")
         cls()
         print("\n\t\tProgramando utilizando o paradigma da indução ...\n")
         print(
-            "A nível de código, resolver esse problema com indução recursiva é muito simples: crie uma função que tem como parâmetro o número de discos a serem empilhados, e os nomes das torres: origem, destino e auxiliar")
+            "A nível de código, resolver esse problema com indução recursiva é muito simples: crie uma função que tem como parâmetro o número de discos a serem empilhados, e os nomes das torres: origem, destino e auxiliar.")
         print(
             "Na função verifique se o problema está no caso base, se estiver, mostre que moveu os discos do pino de origem pro destino, e retorne a função.")
         print(
-            "Caso contrário, chame novamente a função, enviando os discos restantes, e trocando o pino auxiliar com o destino, de modo a satisfazer o passo 2")
-        print("Mostre na tela que os n-1 pinos da origem foram transferidos para o pino auxiliar")
+            "Caso contrário, chame novamente a função, enviando os discos restantes, e trocando o pino auxiliar com o destino, de modo a satisfazer o passo 2.")
+        print("Mostre na tela que os n-1 pinos da origem foram transferidos para o pino auxiliar.")
         print(
-            "Por fim, chame novamente a função, mandando os n-1 pinos, trocando o pino auxiliar com o pino de origem, de modo a reorganizar os pinos e satisfazer a condição 2.1")
+            "Por fim, chame novamente a função, mandando os n-1 pinos, trocando o pino auxiliar com o pino de origem, de modo a reorganizar os pinos e satisfazer a condição 2.1.")
         input("\n\nAperte Qualquer tecla para continuar...")
         cls()
     else:
@@ -62,6 +61,12 @@ def comoFunciona():
         time.sleep(0.75)
         cls()
         return
+    
+def criaPilha(tamanho):
+    for i in range(tamanho):
+        A.append(tamanho)
+        tamanho = tamanho-1
+    return A
 
 
 if __name__ == '__main__':
@@ -86,10 +91,33 @@ if __name__ == '__main__':
             t1 = input("Digite o nome da Torre de Origem: ")
             t2 = input("Digite o nome da Torre de Destino: ")
             t3 = input("Digite o nome da Torre Auxiliar: ")
-            Hanoi.hanoi(int(discs), t1, t2, t3)
-            print("\nHanoi Concluído com Sucesso!\n")
-            input("\n\nAperte Qualquer tecla para continuar...")
             cls()
+          
+            print("===== COMO DESEJA A EXIBIÇÃO? =====\n")
+            print("1 - Linhas de transição\n")
+            print("2 - Pilhas de transição\n")
+            print("===================================")
+            option = input("Digite Sua opção: ")
+            cls()
+            if option == "1":
+                Hanoi.hanoi(int(discs), t1, t2, t3)
+                input("\n\nAperte Qualquer tecla para continuar...")
+                cls()
+            elif option == "2":
+                tam = int(discs)
+                A = []
+                B = []
+                C = []
+                A = criaPilha(int(discs))
+                Hanoi.hanoiPilha(tam, A, B, C, t1, t2, t3)
+                print("\nHanoi concluído com sucesso!")
+                input("\n\nAperte Qualquer tecla para continuar...")
+                cls()
+            else: 
+              cls()
+              print("Opção Inválida!")
+              time.sleep(0.75)
+              cls()
         elif option == "2":
             cls()
             comoFunciona()

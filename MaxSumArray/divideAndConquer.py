b542b5693bd173f0460e0494d84e19270638e449
Gustavo Teixeira Magalhães <gutemagae@gmail.com>
2023-10-24 18:06:04 -0300

    correções iniciais e função soma

--- MaxSumArray/divideAndConquer.py
+++ MaxSumArray/divideAndConquer.py
@@ -2,25 +2,29 @@ class DivConquer:
     def __init__(self, array):
         self.array = array
         self.maxsum = []
-        self.soma = int()
+        self.sum = int(0)
 
-    @staticmethod
-    def soma(array):
+    def soma(self, subarray):
         soma = 0
-        for i in array:
+        for i in subarray:
             soma += i
+        if soma > self.sum:
+            self.sum = soma
+            self.maxsum = subarray
+            return True
+        else:
+            return False
 
     def divide(self, array=None):
         if array is None:
             array = self.array
+        meio = int(len(array)/2)
+
         if len(array) == 1:
-            if array[0] + self.array.soma > self.array.soma:
-                self.array.soma = self.array.soma + array[0]
-                self.array.maxsum.append(array[0])
-                return self.maxsum
-            else:
+            print("\t",array)
+            if self.soma(array):
                 return self.maxsum
         else:
             print(array)
-            self.divide(array[:(len(array) / 2)])
-            self.divide(array[(len(array) / 2):])
+            self.divide(array[:meio])
+            self.divide(array[meio:])
